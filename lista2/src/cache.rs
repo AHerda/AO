ea7d01f3943112d863b7218dba851de99a53eefb
Adrian Herda <adrianherda@gmail.com>
2025-04-05 21:42:26 +0200

    lfu rand rma all done

--- lista2/src/cache.rs
+++ lista2/src/cache.rs
@@ -1,4 +1,8 @@
-use std::{collections::{HashMap, VecDeque}};
+use rand::prelude::*;
+use std::{
+    collections::{HashMap, VecDeque},
+    hash::Hash,
+};
 
 pub enum CacheType {
     Fifo,
@@ -10,17 +14,19 @@ pub enum CacheType {
 }
 
 pub struct Cache<T>
-    where T: Eq
+where
+    T: Eq + Hash + Clone,
 {
     size: usize,
     pub data: VecDeque<T>,
     cache_type: CacheType,
     lfu_counter: Option<HashMap<T, usize>>,
+    rma_marker: Option<Vec<bool>>,
 }
 
-
 impl<T> Cache<T>
-    where T: Eq
+where
+    T: Eq + Hash + Clone,
 {
     pub fn new(size: usize, cache_type: Option<CacheType>) -> Self {
         Cache {
@@ -30,13 +36,17 @@ impl<T> Cache<T>
                 Some(CacheType::Lfu) => Some(HashMap::new()),
                 _ => None,
             },
+            rma_marker: match cache_type {
+                Some(CacheType::Rma) => Some(Vec::with_capacity(size)),
+                _ => None,
+            },
             cache_type: cache_type.unwrap_or(CacheType::Fifo),
         }
     }
 
     pub fn get_page(&mut self, page: T) -> usize {
         if self.data.contains(&page) {
-            self.update_page(page);
+            self.update_page(&page);
             0
         } else {
             self.add_page(page);
@@ -44,10 +54,12 @@ impl<T> Cache<T>
         }
     }
 
-    fn update_page(&mut self, page: T) {
+    fn update_page(&mut self, page: &T) {
         match self.cache_type {
             CacheType::Lru => self.lru_update(page),
-            _ => ()
+            CacheType::Lfu => self.lfu_update(page),
+            CacheType::Rma => self.rma_update(page),
+            _ => (),
         }
     }
 
@@ -56,15 +68,9 @@ impl<T> Cache<T>
             CacheType::Fifo => self.fifo_add(page),
             CacheType::Fwf => self.fwf_add(page),
             CacheType::Lru => self.lru_add(page),
-            CacheType::Lfu => {
-                // Implement LFU logic
-            }
-            CacheType::Rand => {
-                // Implement Random logic
-            }
-            CacheType::Rma => {
-                // Implement RMA logic
-            }
+            CacheType::Lfu => self.lfu_add(page),
+            CacheType::Rand => self.rand_add(page),
+            CacheType::Rma => self.rma_add(page),
         }
     }
 
@@ -89,16 +95,99 @@ impl<T> Cache<T>
         self.data.push_front(page);
     }
 
-    fn lru_update(&mut self, page: T) {
-        let pos = self.data.iter().position(|x| *x == page).expect("Err 1\nPage not found");
-        let page = self.data.remove(pos).expect(format!("Err 2\nError removing page at index {}", pos).as_str());
+    fn lru_update(&mut self, page: &T) {
+        let pos = self
+            .data
+            .iter()
+            .position(|x| x == page)
+            .expect("Err 1\nPage not found");
+        let page = self
+            .data
+            .remove(pos)
+            .expect(format!("Err 2\nError removing page at index {}", pos).as_str());
         self.data.push_front(page);
     }
 
     fn lfu_add(&mut self, page: T) {
         if self.data.len() == self.size {
-            // Implement LFU eviction logic
+            let (pos, _) = self
+                .data
+                .iter()
+                .enumerate()
+                .min_by_key(|(_, x)| self.lfu_counter.as_ref().unwrap()[x])
+                .unwrap();
+            self.data.remove(pos);
+        }
+        self.lfu_update(&page);
+        self.data.push_back(page);
+    }
+
+    fn lfu_update(&mut self, page: &T) {
+        self.lfu_counter
+            .as_mut()
+            .unwrap()
+            .entry(page.clone())
+            .and_modify(|counter| *counter += 1)
+            .or_insert(0);
+    }
+
+    fn rand_add(&mut self, page: T) {
+        if self.data.len() == self.size {
+            self.data.remove(rand::random_range(..self.size));
         }
         self.data.push_back(page);
+    }
+
+    fn rma_add(&mut self, page: T) {
+        if self.data.len() == self.size {
+            let mut unmarked_count = self
+                .rma_marker
+                .as_ref()
+                .unwrap()
+                .into_iter()
+                .filter(|&marker| !marker)
+                .count();
+
+            if unmarked_count == 0 {
+                self.rma_marker
+                    .as_mut()
+                    .unwrap()
+                    .into_iter()
+                    .for_each(|marker| *marker = false);
+                unmarked_count = self.size;
+            }
+
+            let unmarked_pos = rand::random_range(..unmarked_count);
+            let (pos, _) = self
+                .rma_marker
+                .as_ref()
+                .unwrap()
+                .into_iter()
+                .enumerate()
+                .filter(|(_, &marker)| !marker)
+                .nth(unmarked_pos)
+                .expect(
+                    format!(
+                        "Err 4\nUnmarked index {} too big for {} unmarked found",
+                        unmarked_pos, unmarked_count
+                    )
+                    .as_str(),
+                );
+
+            self.data.remove(pos);
+            self.rma_marker.as_mut().unwrap().remove(pos);
+
+            self.data.push_back(page);
+            self.rma_marker.as_mut().unwrap().push(false);
+        }
+    }
+
+    fn rma_update(&mut self, page: &T) {
+        let pos = self
+            .data
+            .iter()
+            .position(|x| x == page)
+            .expect("Err 3\n Page Not found in cache");
+        self.rma_marker.as_mut().unwrap()[pos] = true;
     }
 }
